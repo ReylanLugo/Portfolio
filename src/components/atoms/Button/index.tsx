03f26fb8d546b13630fcc3c88d6faa374f1422bd
Reylan Lugo <ClockR13@gmail.com>
2023-11-21 18:09:36 -0400

    Button v2 with a lots config

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,27 +1,43 @@
 interface ButtonProps {
     className?: string;
     type?: "outline" | "fill";
-    color: "primary" | "secondary";
+    rounded?: "md" | "lg" | "sm" | "xl" | "2xl" | "3xl" | "full" | "none";
+    color: "primary" | "secondary" | "gray";
     onClick?: () => void;
     children: React.ReactNode;
 }
-
-function Button({ type = "outline", className, color, onClick, children }: ButtonProps) {
-    let style;
-    if (type === "outline") {
-        style = "ring-1  rounded-md";
+const colorStyle = {
+    primary: {
+        fill: "bg-teal-500 text-white hover:bg-teal-600",
+        outline: "ring-1 ring-teal-500 text-teal-500 hover:bg-teal-500 hover:text-white",
+    },
+    secondary: {
+        fill: "bg-indigo-500 text-white hover:bg-indigo-600",
+        outline: "ring-1 ring-indigo-500 text-indigo-500 hover:bg-indigo-500 hover:text-white",
+    },
+    gray: {
+        fill: "bg-gray-500 text-white hover:bg-gray-600",
+        outline: "ring-1 ring-gray-500 text-white hover:bg-gray-500",
     }
+}
+const roundedStyle = {
+    md: "rounded-md",
+    lg: "rounded-lg",
+    sm: "rounded-sm",
+    xl: "rounded-xl",
+    "2xl": "rounded-2xl",
+    "3xl": "rounded-3xl",
+    full: "rounded-full",
+    none: "rounded-none",
+}
 
-    let colorStyle;
-    if (color === "primary") {
-        colorStyle = type === "outline" ? "ring-teal-500 text-teal-500 hover:bg-teal-500 hover:text-white" : "bg-teal-500 text-white hover:bg-teal-600 rounded-md";
-    } else if (color === "secondary") {
-        colorStyle = type === "outline" ? "ring-indigo-500 text-gray-500 hover:bg-indigo-500 hover:text-white" : "bg-indigo-500 text-white hover:bg-indigo-600 rounded-md";
-    }
+function Button({ type = "outline", rounded = "md", className="", color, onClick, children }: ButtonProps) {
+
+    const style = `${colorStyle[color][type]} ${roundedStyle[rounded]} ${className}`
 
     return ( 
     <button 
-        className={`${style} ${className} ${colorStyle}`}
+        className={`${style}`}
         onClick={onClick}>
             {children}
     </button> );
